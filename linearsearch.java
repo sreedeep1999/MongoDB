2f944d0c4b66b1f469d054135b1613a1fd593503
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-07-26 13:28:43 +0530

    Add files via upload

---
+++ linearsearch.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class LinearSearch
+{
+    public static void main(String[]args)
+    {
+        int c,n,search,array[];
+        Scanner in=new Scanner(System.in)
+        n=in.nextInt();
+        array=new int[n];
+        System.out.println("enter those "+n+" elements");
+         
+
+    }
+}
